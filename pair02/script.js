5195fe884efbe16fa3b826d6618ecbe258898210
Yehor Ornatskyi <e.ornatsky@gmail.com>
2026-09-04 12:01:48 +0300

    Add files via upload

---
+++ pair02/script.js
@@ -0,0 +1,109 @@
+// > < >= <= == === !== !=
+
+// let a,b;
+// a="1"
+// b=1;
+// console.log(a === b)
+
+// let temp = Number(prompt('Enter a temperature'));
+// let result;
+// if (temp < 0) {
+//     result = "холодно"
+// }
+// else if (temp > 25) {
+//     result = "жарко"
+// }
+// else if (temp > 0) {
+//     result = "нормально"
+// }
+// alert(result);
+
+//-----------------
+//Парне непарне
+// let number=prompt("Enter a number");
+// let ostacha=number%2;
+// let result;
+// if (ostacha == 0){
+//     result="парне"
+// }
+// else if (ostacha==1){
+//     result="непарне"
+// }
+// alert(result);
+
+
+//---------------
+
+// const login="admin";
+// const password="12345";
+//
+// let user_login=prompt("Enter your username");
+// let user_password=prompt("Enter your password")
+//
+// if (user_login ===login && user_password === password){
+//     alert("Доступ дозволено");
+// }
+// else{
+//     alert("Доступ заборонено")
+// }
+
+
+
+// post 100
+// courier 200
+// pickup 0
+
+
+// let deliverytype = prompt('What type of delivery type?');
+// let cost =0;
+// switch (deliverytype) {
+//     case 'post':
+//         cost=100;
+//         break;
+//
+//     case 'courier':
+//         cost=200;
+//         break;
+//     case 'pickup':
+//         cost=0;
+//         break;
+//     default:
+//         cost="n|a"
+// }
+// console.log(cost);
+
+
+
+
+
+//----------------------------------------------------------------
+// let weekday = prompt('What is your day Number?');
+// let day;
+// switch (weekday) {
+//     case '1':
+//         day="Monday";
+//         break;
+//     case '2':
+//         day="Tuesday";
+//         break;
+//     case '3':
+//         day="Wednesday";
+//         break;
+//     case '4':
+//         day="Thursday";
+//         break;
+//     case '5':
+//         day="Friday";
+//         break;
+//     case '6':
+//         day="Saturday";
+//         break;
+//     case '7':
+//         day="Sunday";
+//         break;
+//
+//
+// }
+// console.log(day);
+
+//----------------------------------------------------------------
