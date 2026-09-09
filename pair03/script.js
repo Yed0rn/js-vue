266189ad2afc23b07390470356810d41841fc86f
Yehor Ornatskyi <e.ornatsky@gmail.com>
2026-09-09 11:10:24 +0300

    Add files via upload

---
+++ pair03/script.js
@@ -0,0 +1,88 @@
+// let age=Number(prompt("How old are you?"));
+// let access = confirm("Are you accessed?");
+// if (age>=18 && access===True){
+//     alert("Welcome in");
+// } else{
+//     alert("Access denied.");
+// }
+
+
+
+
+// let role = prompt("What is your access level?");
+// if (role === "admin" || role === "teacher") {
+//     alert("Welcome in");
+// } else{
+//     alert("access denied");
+// }
+
+
+// let login = confirm("Are you registered?");
+// let age = Number(prompt("Enter your age"));
+// if (login===true){
+//     if (age>=8){
+//         alert("Access allowed");
+//     } else{
+//         alert("Access denied");
+//     }
+// } else{
+//     alert("Not logged in");
+// }
+
+
+// let age=Number(prompt("Enter your age"));
+//
+// if(age<6){
+//     alert("Дитина");
+// } else if(age>=6 && age<=13){
+//     alert("Школяр")
+// } else if(age>=14 && age<18){
+//     alert("Підліток")
+// } else {
+//     alert("Дорослий")
+// }
+
+// if(age>=18){
+//     alert("Дорослий");
+// } else if(age>=14){
+//     alert("Підліток")
+// } else if(age>=6){
+//     alert("Школяр")
+// } else {
+//     alert("Дитина")
+// }
+
+
+// let age=Number(prompt("Enter your age"));
+// if (age>18){
+//     alert("access granted");
+// } else if (age>=16 && age<=18){
+//     let access = confirm("Are you accessed?");
+//     if (access){
+//         alert("access granted");
+//     }else{
+//         alert("access denied");
+//     }
+// }else{
+//     alert("access denied");
+// }
+
+
+// const sale1=0.1
+// let name=prompt("Назва товару?");
+// let quantity=Number(prompt("Кількість товару?"));
+// let price = Number(prompt("Ціна товару?"));
+// let sign=confirm("Are you signed in?")
+// let promo=prompt("Have promo?")
+// const promo2='sale';
+// let VIP=confirm("status?")
+// if (price*quantity>=1000 &&sign === true &&(promo===promo2 || VIP===true)){
+//     alert(`Price with sale:${price*quantity*(1-sale1)}$`)
+// } else{
+//     alert(`No sale price ${price*quantity}$`)
+// }
+
+
+
+
+
